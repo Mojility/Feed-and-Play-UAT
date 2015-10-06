d2cff711236a2858d06548573b4e60f5cd8fd213
Martin Ristov <martinristov1@gmail.com>
2015-10-06 17:00:21 -0400

    update team view

--- app/js/controllers/person.js
+++ app/js/controllers/person.js
@@ -5,7 +5,7 @@ var controllersModule = require('./_index');
 /**
  * @ngInject
  */
-function PersonController($stateParams, peopleService) {
+function PersonController($stateParams, peopleService, sessionService, teamService) {
 
     // ViewModel
     var vm = this;
@@ -31,9 +31,45 @@ function PersonController($stateParams, peopleService) {
 
         vm.currentUserName = vm.person.first_name + " " + vm.person.last_name;
 
+        vm.memberships = sessionService.memberships;
+
+        vm.getTeams(vm.person.id)
+       // console.log(vm.teams);
+      //  vm.getVideos( vm.getTeams(vm.person.id));
+    };
+
+
+    vm.getTeams = function (id) {
+
+        vm.teams = [];
+
+        vm.memberships.forEach(function (member, value) {
+
+            if (member.person_id == id) {
+                vm.teams.splice(value, 0, member.team_id);
+                //console.log("true");
+
+            }
+
+
+        });
+
+
+
+
+
+    };
+
+    vm.getVideos = function (id) {
+
+        vm.team = teamService.getTeam(id);
+        console.log(vm.team);
+        vm.videos = vm.team.video_id;
+        console.log(vm.videos);
+        return vm.videos;
 
     };
 
 }
 
-controllersModule.controller('PersonController', ['$stateParams', 'PeopleService', PersonController]);
+controllersModule.controller('PersonController', ['$stateParams', 'PeopleService', 'SessionService','TeamService', PersonController]);
