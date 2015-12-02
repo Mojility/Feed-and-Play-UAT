791dd346985d7a50db8318becc30ca08e579f3e2
Stacey Vetzal <stacey@vetzal.com>
2015-12-02 17:08:03 -0500

    explicit variable names

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -7,18 +7,18 @@ When(/^I click the login link$/) do
     driver.find_element(css: 'a.login').click
 end
 
-When(/^I enter my username "([^"]*)"$/) do |keyword|
-    driver.find_element(css: '#email').send_keys(keyword)
+When(/^I enter my username "([^"]*)"$/) do |email|
+    driver.find_element(css: '#email').send_keys(email)
 end
 
-When(/^I enter my password "([^"]*)"$/) do |keyword|
-    driver.find_element(css: '#password').send_keys(keyword)
+When(/^I enter my password "([^"]*)"$/) do |password|
+    driver.find_element(css: '#password').send_keys(password)
 end
 
 When(/^I click the log in button$/) do
     driver.find_element(css: 'button[type=submit]').click
 end
 
-Then(/^I should see my profile page with my stagename "([^"]*)"$/) do |keyword|  
-    expect(driver.find_elements(css: '.stagename')).to eq(keyword)
+Then(/^I should see my profile page with my stagename "([^"]*)"$/) do |stage_name|
+    expect(driver.find_elements(css: '.stagename')).to eq(stage_name)
 end
