c5573effd6132b54d61bbe94f802661b5719e808
Stacey Vetzal <stacey@vetzal.com>
2015-12-09 14:11:41 -0500

    adding database initialization before each scenario

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,12 +4,20 @@ def site_url
   ENV['SITE_URL'] || 'http://localhost:3001'
 end
 
+def init_db
+  system('cd ../Feed-and-Play-Server && docker-compose run api bundle exec rake db:seed')
+end
+
 def driver
   @driver ||= Selenium::WebDriver.for :firefox#, profile: Selenium::WebDriver::Firefox::Profile.new
 end
 
 def wait
   @wait ||= Selenium::WebDriver::Wait.new(timeout: 30)
+end
+
+Before do
+  init_db
 end
 
 After do
